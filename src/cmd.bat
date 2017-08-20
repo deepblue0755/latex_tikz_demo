76580ea0690a5ec58ee7224b0f2d5af0030bd1c9
deepblue0755 <mianbo.huang@hotmail.com>
2017-08-20 17:19:11 +0800

    update from t430:add more examples from texexample.net

--- src/cmd.bat
+++
@@ -1,8 +0,0 @@
-:: pdflatex -file-line-error -halt-on-error -output-directory=./output ./junction-diagram.tex
-::pdflatex -file-line-error -halt-on-error -output-directory=./output ./geogebra-export.tex
-set TEX_SRC_FILE=.\bar-chart.tex
-set PDFLATEX=D:\texlive\2016\bin\win32\pdflatex.exe
-%PDFLATEX% -file-line-error -halt-on-error -output-directory=./output %TEX_SRC_FILE%
-
-
-@PAUSE
