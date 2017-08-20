feb7f81d750856039b8a44be5269c3133b82893c
deepblue0755 <mianbo.huang@hotmail.com>
2017-08-20 22:23:05 +0800

    update from t430: add more examples from texexample.tex

--- src/copy_makefile.sh
+++ src/copy_makefile.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-
 if [ $# = 1 ]; then
     echo "try to find folder that don't contain pdf file"
     for folder in * 
     do
         if [ -d "$folder" ];then
-            cd $foler
+            cd $folder
             file="${folder%%.pdf}.pdf"
-            echo $file
-            if [ ! -f "$file" ]; then
+            if [ ! -f $file ]; then
                 echo "no pdf files $file  found in $folder"
             fi
             cd ../
@@ -25,6 +23,7 @@ do
         if [ ! -f  "Makefile" ]; then
             echo "$folder do not contain Makefile"
             cp -iv ../smart-diagram/Makefile ./
+            make 
         fi
         cd ../
     fi
